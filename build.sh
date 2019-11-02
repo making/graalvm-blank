7ea599565677a8dccd35e8ff699f9db25372ee8f
Toshiaki Maki <tmaki@pivotal.io>
2019-11-03 03:09:18 +0900

    Update

--- build.sh
+++ build.sh
@@ -3,16 +3,11 @@
 ./mvnw clean package
 rm -rf build
 unzip target/graalvm-blank-*.jar -d build
-# docker run --rm -p 8080:8080 \
-#            -v "$PWD":/usr/src \
-#            -w /usr/src \
-#            oracle/graalvm-ce:1.0.0-rc15 \
-
 
 mvn -N io.takari:maven:wrapper
 docker run --rm \
            -v "$PWD":/usr/src \
            -v "$HOME/.m2":/root/.m2 \
            -w /usr/src \
-           oracle/graalvm-ce:1.0.0-rc15 \
+           oracle/graalvm-ce:19.2.0.1 \
            ./mvnw package -Pgraal
